4f5fae5759a45bc811b0ed3d02c80e4a08747db9
Giulia <giulia.saunders@alu.ufc.br>
2024-05-26 18:09:52 -0300

    Desafio implementado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("123", "nokia1500", "54678", 123456);
+nokia.Ligar();
+nokia.InstalarAplicativo("Telegram");
+
+Iphone iphone = new Iphone("567", "IphoneX", "123456", 56789);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Spotify");
